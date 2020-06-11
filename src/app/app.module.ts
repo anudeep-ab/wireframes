05632011134a77890a78ecabba31ac8e98b8a762
Anudeep <anudeep.uidev@gmail.com>
2020-06-11 09:25:22 -0500

    wireframe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TransactionComponent } from './transaction/transaction.component';
+
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+
+import { MatExpansionModule } from '@angular/material/expansion';
+import { Transaction2Component } from './transaction2/transaction2.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, TransactionComponent, Transaction2Component],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    CollapseModule.forRoot(),
+    MatExpansionModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

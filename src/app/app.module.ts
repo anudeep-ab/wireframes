b06a461b62e2c0ac4445ae4143f639f518393823
Anudeep <anudeep.uidev@gmail.com>
2020-06-12 02:47:37 -0500

    screen 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,15 +10,32 @@ import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 import { MatExpansionModule } from '@angular/material/expansion';
 import { Transaction2Component } from './transaction2/transaction2.component';
+import { Screen3Component } from './screen3/screen3.component';
+
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [AppComponent, TransactionComponent, Transaction2Component],
+  declarations: [
+    AppComponent,
+    TransactionComponent,
+    Transaction2Component,
+    Screen3Component,
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     CollapseModule.forRoot(),
     MatExpansionModule,
+    MatListModule,
+    MatDividerModule,
+    MatChipsModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/screen3/screen3.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-screen3',
+  templateUrl: './screen3.component.html',
+  styleUrls: ['./screen3.component.scss'],
+})
+export class Screen3Component implements OnInit {
+  opened: boolean;
+  constructor() {}
+
+  ngOnInit(): void {}
+}
